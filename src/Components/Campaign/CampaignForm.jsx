d430a420e143b03c2d2f49c29077a57e891ab0c6
volalexey <sempayfox@gmail.com>
2025-04-17 01:51:54 +0300

    add styles for campaign form

--- src/Components/Campaign/CampaignForm.jsx
+++ src/Components/Campaign/CampaignForm.jsx
@@ -1,20 +1,17 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useContext, useEffect, useState } from 'react';
 import { CampaignsContext } from '../../Context/CampaignsContext';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../Context/UserContext';
+import './Style/CampaignForm.css';
 
-export const CampaignForm = ({type}) => {
+export const CampaignForm = ({ type }) => {
     const navigate = useNavigate();
-    const {
-        register,
-        handleSubmit,
-        setValue
-    } = useForm();
+    const { register, handleSubmit, setValue } = useForm();
     const [campaign, setCampaign] = useState({});
 
-    const {user} = useContext(UserContext);
-    const {addCampaign, getCampaignById, editCampaign} = useContext(CampaignsContext);
+    const { user } = useContext(UserContext);
+    const { addCampaign, getCampaignById, editCampaign } = useContext(CampaignsContext);
 
     useEffect(() => {
         fetchCampaign();
@@ -23,30 +20,24 @@ export const CampaignForm = ({type}) => {
     const fetchCampaign = async () => {
         if (type !== 'add') {
             const existingCampaign = await getCampaignById(type);
-            console.log(existingCampaign);
-
             if (existingCampaign) {
                 setCampaign(existingCampaign);
-                
                 Object.keys(existingCampaign).forEach(key => {
                     setValue(key, existingCampaign[key]);
                 });
             }
         }
-    }
+    };
 
-    const onSubmit = (values) =>{
-        if(type === 'add'){
-            console.log("adding campaign");
+    const onSubmit = (values) => {
+        if (type === 'add') {
             handleAdd(values);
-        }
-        else{
-            console.log("editing campaign");
+        } else {
             handleEdit(values);
         }
-    }
+    };
 
-    const handleAdd = (values) =>{
+    const handleAdd = (values) => {
         const campaign = {
             name: values.name,
             description: values.description,
@@ -56,12 +47,10 @@ export const CampaignForm = ({type}) => {
             imageUrl: values.imageUrl,
             socialUrls: values.socialUrls,
             creatorId: user.id
-        }
+        };
         addCampaign(campaign);
-        console.log(campaign);
-
         navigate('/campaigns');
-    }
+    };
 
     const handleEdit = (values) => {
         const updatedCampaign = { ...campaign, ...values };
@@ -69,43 +58,47 @@ export const CampaignForm = ({type}) => {
         navigate(`/campaigns/${updatedCampaign.id}`);
     };
 
-    return(
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" placeholder='Name' id='name' {...register("name", {required:true})}/>
-            </div>
-            <div>
-                <label htmlFor="description">Description</label>
-                <textarea type="text" placeholder='Input some information about campaign...' id='description' {...register("description", {required:true, maxLength:500})}/>
-            </div>
-            <div>
-                <label htmlFor="category" className="form-label"> Group Type </label>
-                <select
-                    id="category"
-                    {...register("category")}
-                >
-                    <option value="Blabalbla">Blabalbla</option>
-                    <option value="Blebleble">Blebleble</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor="destination">Destination</label>
-                <input type='number' id='destination' {...register("destination", {required:true})}/>
-            </div>
-            <div>
-                <label htmlFor="imageUrl">Image url</label>
-                <input type='text' id='Image' {...register("imageUrl", {required:true})}/>
-            </div>
-            <div>
-                <label htmlFor="socialUrls">Social Urls</label>
-                <input type='text' id='Images' {...register("socialUrls", {required:true})}/>
-            </div>
-            <div>
-                <button type='submit'>{type === 'add' ? "Create Campaign" : "Edit Campaign"}</button>
-            </div>
-        </form>
-    )
-}
+    return (
+        <div className="div-campaign-form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="div-campaign-form-container">
+                    <h1>{type === 'add' ? "Створити кампанію" : "Редагувати кампанію"}</h1>
+                    <hr />
+                    <div>
+                        <label htmlFor="name">Назва</label>
+                        <input className="input-campaign" type="text" id="name" placeholder="Name" {...register("name", { required: true })} />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Опис</label>
+                        <textarea className="input-campaign" id="description" placeholder="Input some information..." {...register("description", { required: true, maxLength: 500 })}></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="category">Категорія</label>
+                        <select className="input-campaign" id="category" {...register("category")}>
+                            <option value="Blabalbla">Blabalbla</option>
+                            <option value="Blebleble">Blebleble</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="destination">Ціль</label>
+                        <input className="input-campaign" type="number" id="destination" {...register("destination", { required: true })} />
+                    </div>
+                    <div>
+                        <label htmlFor="imageUrl">Image URL</label>
+                        <input className="input-campaign" type="text" id="imageUrl" {...register("imageUrl", { required: true })} />
+                    </div>
+                    <div>
+                        <label htmlFor="socialUrls">Social URLs</label>
+                        <input className="input-campaign" type="text" id="socialUrls" {...register("socialUrls", { required: true })} />
+                    </div>
+                    <hr />
+                    <button className="btn btn-campaign-submit" type="submit">
+                        {type === 'add' ? "Створити" : "Оновити"}
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default CampaignForm;
